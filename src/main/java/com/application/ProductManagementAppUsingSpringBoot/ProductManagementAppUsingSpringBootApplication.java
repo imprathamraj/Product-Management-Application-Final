21fa3505f151f7e1f4e085cb038a742e09046f65
imprathamraj <rjpratham05@gmail.com>
2025-03-23 23:32:11 +0530

    [Pratham Raj] Add. Main.java files

---
+++ src/main/java/com/application/ProductManagementAppUsingSpringBoot/ProductManagementAppUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.application.ProductManagementAppUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementAppUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementAppUsingSpringBootApplication.class, args);
+	}
+
+}
